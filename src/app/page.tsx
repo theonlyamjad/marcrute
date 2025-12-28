9dcef9d197bd8fdc4deaec9ff74cde53b43cfd6b
theonlyamjad <amjadmessi2004@gmail.com>
2025-12-28 16:32:39 +0100

    feat: full landing page and it's blocks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
-import Image from "next/image";
+import Bgpattern from "@/components/landing/Bgpattern";
+import Navbar from "@/components/landing/Navbar"; 
+import Hero from "@/components/landing/Hero";
+import Categories from "@/components/landing/Categories";
+import HowItWorks from "@/components/landing/HowItWorks";
+import Stats from "@/components/landing/Stats";
+import CTA from "@/components/landing/CTA";
+import Footer from "@/components/landing/Footer";
+
 
 export default function Home() {
   return (
-   <>
-   
-   
-   </>
+  <>
+   <Bgpattern/>
+   <Navbar/>
+   <Hero/>
+   <Categories/>
+   <HowItWorks/>
+   <Stats/>
+   <CTA/>
+   <Footer/>
+  </>
   );
 }
