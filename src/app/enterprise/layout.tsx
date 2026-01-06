818622f10eab5df00efee71cf24a7618d7cb80ea
Mounir-abaddah <mounirabaddah6@gmail.com>
2026-01-06 22:44:38 +0100

    :sparkles: feat: implement role verification for EnterpriseLayout to ensure user is an institution

--- src/app/enterprise/layout.tsx
+++ src/app/enterprise/layout.tsx
@@ -1,12 +1,16 @@
+import { requireRole } from "@/lib/auth";
 
-export default function RootLayout({
+export default async function EnterpriseLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // Vérifier que l'utilisateur est une institution
+  await requireRole("Institution");
+
   return (
-        <div>
-            {children}
-        </div>
+    <div>
+      {children}
+    </div>
   );
 }
